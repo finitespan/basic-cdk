626e65cd4aaf3e30d7701dd3a31b888c3ab07337
Scott Lowrey <scott@finite.com>
2023-06-21 08:48:11 -0500

    add s3 removal policy

--- basic_cdk/basic_cdk_stack.py
+++ basic_cdk/basic_cdk_stack.py
@@ -1,5 +1,6 @@
 from aws_cdk import (
     App,
+    RemovalPolicy,
     Stack,
     aws_s3 as s3
 )
@@ -10,4 +11,7 @@ class BasicCdkStack(Stack):
 
     def __init__(self, scope: App, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-        bucket = s3.Bucket(self, "TestBucket", versioned=True)
+        bucket = s3.Bucket(self, "TestBucket",
+                           versioned=True,
+                           removal_policy=RemovalPolicy.DESTROY,
+                           auto_delete_objects=True)
