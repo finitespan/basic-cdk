46d2bdbc6937e8a252203c0f3ebda43605011b51
Scott Lowrey <scott@finite.com>
2023-06-21 07:02:41 -0500

    add s3 bucket to basic stack

--- basic_cdk/basic_cdk_stack.py
+++ basic_cdk/basic_cdk_stack.py
@@ -1,19 +1,13 @@
 from aws_cdk import (
-    # Duration,
+    App,
     Stack,
-    # aws_sqs as sqs,
+    aws_s3 as s3
 )
 from constructs import Construct
 
+
 class BasicCdkStack(Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: App, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "BasicCdkQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        bucket = s3.Bucket(self, "TestBucket", versioned=True)
